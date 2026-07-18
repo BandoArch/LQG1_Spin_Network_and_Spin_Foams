1ab298559dda77b3509fdea082b789153545d295
Bando <eliudmugo360@gmail.com>
2026-07-18 12:28:39 +0300

    Plots for SpinFoams and rename for SpinNetwork

--- SpinFoams.py
+++ SpinFoams.py
@@ -1,17 +1,18 @@
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
+import matplotlib.pyplot as plt
 
-# We only need one simulator instance
 sim = AerSimulator()
+
+dynamic_probabilities = []
 
 # 1. The Time Loop
 for t in range(1, 6):
-    # Create a FRESH universe at the start of each time step!
+    # Create 3 new nodes/qubits for each spin state change
     qc = QuantumCircuit(3)
     
-    # 2. Apply our time-steps 't' times
-    for step in range(t):
-        # First, rotate all qubits (Kinetic Step)
+    for step in range(1):
+        # Rotating qubits/nodes
         for qubit in range(3):
             qc.rx(0.2, qubit)
             
@@ -32,5 +33,19 @@ for t in range(1, 6):
     
     # 5. Extract the probability of the '000' state
     prob_000 = counts.get('000', 0) / 1024
-    
+    dynamic_probabilities.append(prob_000)
     print(f"Time Step {t} -> Probability of '000': {prob_000:.3f} | Total Counts: {counts}")
+
+
+
+# Plotting the data
+time_steps = [1, 2, 3, 4, 5]
+ # The '000' state data
+
+plt.plot(time_steps, dynamic_probabilities, marker='o', linestyle='-', color='b')
+plt.title("Quantum Volume Fluctuation (The 'Breathing' Effect)")
+plt.xlabel("Time Step (t)")
+plt.ylabel("Probability of Base Volume State ('000')")
+plt.grid(True)
+
+plt.show()
